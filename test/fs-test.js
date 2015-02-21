af40fdcf418cc38ae346ae2a7bbe5a55c200b635
Alexander Pope <alex@pope-industries.com>
2015-02-21 16:57:49 +0100

    add walk()

--- test/fs-test.js
+++ test/fs-test.js
@@ -5,15 +5,11 @@ var path = require('path')
 	, fsutils = require('..')
 	, indir = fsutils.indir
 	, readdir = fsutils.readdir
-	, readdirSync = readdir.sync
 	, mkdir = fsutils.mkdir
-	, mkdirSync = mkdir.sync
 	, mv = fsutils.mv
-	, mvSync = mv.sync
 	, rm = fsutils.rm
-	, rmSync = rm.sync
 	, cp = fsutils.cp
-	, cpSync = cp.sync;
+	, walk = fsutils.walk;
 
 describe('recur-fs', function () {
 	before(function () {
@@ -78,32 +74,32 @@ describe('recur-fs', function () {
 		});
 		describe('sync', function () {
 			it('should read the contents of a flat directory', function () {
-				var fd = readdirSync(path.resolve('readdir-simple'));
+				var fd = readdir.sync(path.resolve('readdir-simple'));
 				fd.files.should.have.length(4);
 				fd.directories.should.have.length(1);
 			});
 			it('should read the contents of a nested directory', function () {
-				var fd = readdirSync(path.resolve('readdir-nested'));
+				var fd = readdir.sync(path.resolve('readdir-nested'));
 				fd.files.should.have.length(4);
 				fd.directories.should.have.length(3);
 			});
 			it('should ignore files when an ignore pattern is passed', function () {
-				var fd = readdirSync(path.resolve('readdir-simple'), null, /^Class/);
+				var fd = readdir.sync(path.resolve('readdir-simple'), null, /^Class/);
 				fd.files.should.have.length(2);
 				fd.directories.should.have.length(1);
 			});
 			it('should include files when an include pattern is passed', function () {
-				var fd = readdirSync(path.resolve('readdir-simple'), /^Class/);
+				var fd = readdir.sync(path.resolve('readdir-simple'), /^Class/);
 				fd.files.should.have.length(2);
 				fd.directories.should.have.length(1);
 			});
 			it('should skip empty nested directories', function () {
-				var fd = readdirSync(path.resolve('readdir-empty'));
+				var fd = readdir.sync(path.resolve('readdir-empty'));
 				fd.files.should.have.length(1);
 				fd.directories.should.have.length(2);
 			});
 			it('should automatically ignore hidden files', function () {
-				var fd = readdirSync(path.resolve('readdir-empty/empty'));
+				var fd = readdir.sync(path.resolve('readdir-empty/empty'));
 				fd.files.should.have.length(0);
 				fd.directories.should.have.length(1);
 			});
@@ -139,15 +135,15 @@ describe('recur-fs', function () {
 		});
 		describe('sync', function () {
 			it('should create a directory', function () {
-				mkdirSync(path.resolve('mkdir', 'test'));
+				mkdir.sync(path.resolve('mkdir', 'test'));
 				fs.existsSync(path.resolve('mkdir', 'test')).should.be.true;
 			});
 			it('should create a nested directory', function () {
-				mkdirSync(path.resolve('mkdir', 'test', 'test', 'test'));
+				mkdir.sync(path.resolve('mkdir', 'test', 'test', 'test'));
 				fs.existsSync(path.resolve('mkdir', 'test', 'test', 'test')).should.be.true;
 			});
 			it('should create a directory when passed a file', function () {
-				mkdirSync(path.resolve('mkdir', 'test', 'test.txt'));
+				mkdir.sync(path.resolve('mkdir', 'test', 'test.txt'));
 				fs.existsSync(path.resolve('mkdir', 'test')).should.be.true;
 			});
 		});
@@ -212,34 +208,34 @@ describe('recur-fs', function () {
 			it('should move a file to an existing directory', function () {
 				fs.mkdirSync(path.resolve('mv', 'test'));
 				fs.writeFileSync(path.resolve('mv', 'test.txt'), 'blah', 'utf8');
-				mvSync(path.resolve('mv', 'test.txt'), path.resolve('mv', 'test'), false);
+				mv.sync(path.resolve('mv', 'test.txt'), path.resolve('mv', 'test'), false);
 				fs.existsSync(path.resolve('mv', 'test.txt')).should.be.false;
 				fs.existsSync(path.resolve('mv', 'test', 'test.txt')).should.be.true;
 			});
 			it('should move a nested file to an existing directory', function () {
 				fs.writeFileSync(path.resolve('mv', 'test.txt'), 'blah', 'utf8');
-				mvSync(path.resolve('mv', 'test.txt'), path.resolve('mv', 'test'), false);
+				mv.sync(path.resolve('mv', 'test.txt'), path.resolve('mv', 'test'), false);
 				fs.existsSync(path.resolve('mv', 'test.txt')).should.be.false;
 				fs.existsSync(path.resolve('mv', 'test', 'test.txt')).should.be.true;
 			});
 			it('should return the path to the moved file', function () {
 				fs.mkdirSync(path.resolve('mv', 'test'));
 				fs.writeFileSync(path.resolve('mv', 'test.txt'), 'blah', 'utf8');
-				var filepath = mvSync(path.resolve('mv', 'test.txt'), path.resolve('mv', 'test'), false);
+				var filepath = mv.sync(path.resolve('mv', 'test.txt'), path.resolve('mv', 'test'), false);
 				filepath.should.eql(path.resolve('mv', 'test', 'test.txt'));
 			});
 			it('should not return an error when moving a file to a location with an existing file of the same name', function () {
 				fs.mkdirSync(path.resolve('mv', 'test'));
 				fs.writeFileSync(path.resolve('mv', 'test.txt'), 'blah', 'utf8');
 				fs.writeFileSync(path.resolve('mv', 'test', 'test.txt'), 'blah', 'utf8');
-				mvSync(path.resolve('mv', 'test.txt'), path.resolve('mv', 'test'), false);
+				mv.sync(path.resolve('mv', 'test.txt'), path.resolve('mv', 'test'), false);
 				fs.existsSync(path.resolve('mv', 'test.txt')).should.be.true;
 			});
 			it('should ovewrite when moving a file to a location with an existing file of the same name and force=true', function () {
 				fs.mkdirSync(path.resolve('mv', 'test'));
 				fs.writeFileSync(path.resolve('mv', 'test.txt'), 'blah', 'utf8');
 				fs.writeFileSync(path.resolve('mv', 'test', 'test.txt'), 'blah blah', 'utf8');
-				mvSync(path.resolve('mv', 'test.txt'), path.resolve('mv', 'test'), true);
+				mv.sync(path.resolve('mv', 'test.txt'), path.resolve('mv', 'test'), true);
 				fs.existsSync(path.resolve('mv', 'test', 'test.txt')).should.be.true;
 				fs.readFileSync(path.resolve('mv', 'test', 'test.txt'), 'utf8').should.eql('blah');
 			});
@@ -278,12 +274,12 @@ describe('recur-fs', function () {
 		describe('sync', function () {
 			it('should remove a file in the project path', function () {
 				fs.writeFileSync(path.resolve('rm', 'test.txt'), 'blah', 'utf8');
-				rmSync(path.resolve('rm', 'test.txt'));
+				rm.sync(path.resolve('rm', 'test.txt'));
 				fs.existsSync(path.resolve('rm', 'test.txt')).should.be.false;
 			});
 			it('should return an error when attempting to remove a file outside the project path', function () {
 				try {
-					rmSync(path.resolve('..', 'dummy'));
+					rm.sync(path.resolve('..', 'dummy'));
 				} catch (err) {
 					should.exist(err);
 					fs.existsSync(path.resolve('..', 'dummy')).should.be.true;
@@ -291,7 +287,7 @@ describe('recur-fs', function () {
 			});
 			it('should return an error when attempting to remove a file that does not exist', function () {
 				try {
-					rmSync(path.resolve('rm', 'dummy'));
+					rm.sync(path.resolve('rm', 'dummy'));
 				} catch (err) {
 					should.exist(err);
 				}
@@ -310,25 +306,25 @@ describe('recur-fs', function () {
 		});
 		describe('async', function () {
 			it('should copy a file from one directory to another directory', function (done) {
-				cp(path.resolve('main.coffee'), path.resolve('test'), false, function (err, filepath) {
-					fs.existsSync(path.resolve('test', 'main.coffee')).should.be.true
+				cp(path.resolve('main.js'), path.resolve('test'), false, function (err, filepath) {
+					fs.existsSync(path.resolve('test', 'main.js')).should.be.true
 					done()
 				});
 			});
 			it('should copy a file from one directory to a new file name in another directory', function (done) {
-				cp(path.resolve('main.coffee'), path.resolve('test', 'test.coffee'), false, function (err, filepath) {
-					fs.existsSync(path.resolve('test', 'test.coffee')).should.be.true;
+				cp(path.resolve('main.js'), path.resolve('test', 'test.js'), false, function (err, filepath) {
+					fs.existsSync(path.resolve('test', 'test.js')).should.be.true;
 					done();
 				});
 			});
 			it('should copy a file to a new file in the same directory with a new name', function (done) {
-				cp(path.resolve('test', 'main.coffee'), path.resolve('test', 'test2.coffee'), false, function (err, filepath) {
-					fs.existsSync(path.resolve('test', 'test2.coffee')).should.be.true;
+				cp(path.resolve('test', 'main.js'), path.resolve('test', 'test2.js'), false, function (err, filepath) {
+					fs.existsSync(path.resolve('test', 'test2.js')).should.be.true;
 					done();
 				});
 			});
 			it('should not return an error when copying a file to the same directory without a new name', function (done) {
-				cp(path.resolve('test', 'main.coffee'), path.resolve('test'), false, function (err, filepath) {
+				cp(path.resolve('test', 'main.js'), path.resolve('test'), false, function (err, filepath) {
 					should.not.exist(err);
 					done();
 				});
@@ -341,13 +337,13 @@ describe('recur-fs', function () {
 			});
 			it('should only copy the contents of a directory when the source contains a trailing "/"', function (done) {
 				cp(path.resolve('package') + path.sep, path.resolve('test'), false, function (err, filepath) {
-					fs.existsSync(path.resolve('test', 'Class.coffee')).should.be.true;
-					fs.existsSync(path.resolve('test', 'ClassCamelCase.coffee')).should.be.true;
+					fs.existsSync(path.resolve('test', 'Class.js')).should.be.true;
+					fs.existsSync(path.resolve('test', 'ClassCamelCase.js')).should.be.true;
 					done();
 				});
 			});
 			it('should return an error when copying a directory to a file', function (done) {
-				cp(path.resolve('package'), path.resolve('test', 'main.coffee'), false, function (err, filepath) {
+				cp(path.resolve('package'), path.resolve('test', 'main.js'), false, function (err, filepath) {
 					should.exist(err);
 					done();
 				});
@@ -355,40 +351,75 @@ describe('recur-fs', function () {
 		});
 		describe('sync', function () {
 			it('should copy a file from one directory to another directory', function () {
-				cpSync(path.resolve('main.coffee'), path.resolve('test'), false);
-				fs.existsSync(path.resolve('test', 'main.coffee')).should.be.true
+				cp.sync(path.resolve('main.js'), path.resolve('test'), false);
+				fs.existsSync(path.resolve('test', 'main.js')).should.be.true
 			});
 			it('should copy a file from one directory to a new file name in another directory', function () {
-				cpSync(path.resolve('main.coffee'), path.resolve('test', 'test.coffee'), false);
-				fs.existsSync(path.resolve('test', 'test.coffee')).should.be.true;
+				cp.sync(path.resolve('main.js'), path.resolve('test', 'test.js'), false);
+				fs.existsSync(path.resolve('test', 'test.js')).should.be.true;
 			});
 			it('should copy a file to a new file in the same directory with a new name', function () {
-				cpSync(path.resolve('test', 'main.coffee'), path.resolve('test', 'test2.coffee'));
-				fs.existsSync(path.resolve('test', 'test2.coffee')).should.be.true;
+				cp.sync(path.resolve('test', 'main.js'), path.resolve('test', 'test2.js'));
+				fs.existsSync(path.resolve('test', 'test2.js')).should.be.true;
 			});
 			it('should not return an error when copying a file to the same directory without a new name', function () {
 				try {
-					cpSync(path.resolve('main.coffee'), process.cwd(), false);
+					cp.sync(path.resolve('main.js'), process.cwd(), false);
 				} catch (err) {
 					should.not.exist(err);
 				}
 			});
 			it('should copy a directory and it\'s contents from one directory to another directory', function () {
-				cpSync(path.resolve('package'), path.resolve('test'), false);
+				cp.sync(path.resolve('package'), path.resolve('test'), false);
 				fs.existsSync(path.resolve('test', 'package')).should.be.true;
 			});
 			it('should only copy the contents of a directory when the source contains a trailing "/"', function () {
-				cpSync(path.resolve('package') + path.sep, path.resolve('test'), false);
-				fs.existsSync(path.resolve('test', 'Class.coffee')).should.be.true;
-				fs.existsSync(path.resolve('test', 'ClassCamelCase.coffee')).should.be.true;
+				cp.sync(path.resolve('package') + path.sep, path.resolve('test'), false);
+				fs.existsSync(path.resolve('test', 'Class.js')).should.be.true;
+				fs.existsSync(path.resolve('test', 'ClassCamelCase.js')).should.be.true;
 			});
 			it('should return an error when copying a directory to a file', function () {
 				try {
-					cpSync(path.resolve('package'), path.resolve('test', 'main.coffee'), false);
+					cp.sync(path.resolve('package'), path.resolve('test', 'main.js'), false);
 					should.not.be.ok();
 				} catch (err) {
 					should.exist(err);
 				}
+			});
+		});
+	});
+
+	describe('walk', function () {
+		describe('async', function () {
+			it('should visit all resources of a directory and it\'s parents', function (done) {
+				var visits = 0;
+				walk(path.resolve('readdir-nested/src/package'), function (resource, next) {
+					if (~resource.indexOf('.js')) visits++;
+					next(path.basename(path.dirname(resource)) != 'fixtures');
+				}, function (err) {
+					visits.should.equal(4);
+					done();
+				});
+			});
+			it('should allow for early termination', function (done) {
+				var visits = 0;
+				walk('readdir-nested/src/package', function (resource, next) {
+					if (~resource.indexOf('.js')) visits++;
+					next(false);
+				}, function (err) {
+					visits.should.equal(2);
+					done();
+				});
+			});
+		});
+
+		describe('sync', function () {
+			it('should visit all resources of a directory and it\'s parents', function () {
+				var visits = 0;
+				walk.sync(path.resolve('readdir-nested/src/package'), function (resource) {
+					if (resource.indexOf(process.cwd()) == 0 && ~resource.indexOf('.js')) visits++;
+				});
+				visits.should.equal(4);
 			});
 		});
 	});
